2f864d8e8c2fad1bfa1e2125abba3748eb0bd751
bitterhalt <bazz@void.com>
2024-09-15 02:02:43 +0300

    Changes to be committed: 	modified:   .config/river/init 	modified:   .config/river/scripts/mediactrl.sh 	modified:   .config/river/scripts/waybar-toggle.sh

--- .config/river/scripts/mediactrl.sh
+++ .config/river/scripts/mediactrl.sh
@@ -5,59 +5,59 @@
 
 # Play the next track
 play_next() {
-    playerctl next
-    show_music_notification
+  playerctl next
+  show_music_notification
 }
 
 # Play the previous track
 play_previous() {
-    playerctl previous
-    show_music_notification
+  playerctl previous
+  show_music_notification
 }
 
 # Toggle play/pause
 toggle_play_pause() {
-    playerctl play-pause
-    show_music_notification
+  playerctl play-pause
+  show_music_notification
 }
 
 # Stop playback
 stop_playback() {
-    playerctl stop
-    notify-send -e -u low "Playback Stopped"
+  playerctl stop
+  notify-send -e -u low "Playback Stopped"
 }
 
 # Display notification with song information
 show_music_notification() {
-    status=$(playerctl status)
-    if [[ "$status" == "Playing" ]]; then
-        song_title=$(playerctl metadata title)
-        song_artist=$(playerctl metadata artist)
-        notify-send -t 5000 -a playerctrl_notify "Playing:" "$song_title\nby $song_artist" -h string:x-canonical-private-synchronous:volume
-    elif [[ "$status" == "Paused" ]]; then
-        notify-send -t 5000 -a playerctrl_notify "Playback Paused" -h string:x-canonical-private-synchronous:volume
-    fi
+  status=$(playerctl status)
+  if [[ "$status" == "Playing" ]]; then
+    song_title=$(playerctl metadata title)
+    song_artist=$(playerctl metadata artist)
+    notify-send -t 5000 -a playerctrl_notify "Playing:" "$song_title\nby $song_artist" -h string:x-canonical-private-synchronous:volume
+  elif [[ "$status" == "Paused" ]]; then
+    notify-send -t 5000 -a playerctrl_notify "Playback Paused" -h string:x-canonical-private-synchronous:volume
+  fi
 }
 
 # Get media control action from command line argument
 case "$1" in
-    "--nxt")
-        play_next
-        ;;
-    "--prv")
-        play_previous
-        ;;
-    "--play")
-        toggle_play_pause
-        ;;
-    "--stop")
-        stop_playback
-        ;;
-    "--noti")
-        show_music_notification
-        ;;
-    *)
-        echo "Usage: $0 [--noti|--nxt|--prv|--play|--stop]"
-        exit 1
-        ;;
+"--nxt")
+  play_next
+  ;;
+"--prv")
+  play_previous
+  ;;
+"--play")
+  toggle_play_pause
+  ;;
+"--stop")
+  stop_playback
+  ;;
+"--noti")
+  show_music_notification
+  ;;
+*)
+  echo "Usage: $0 [--noti|--nxt|--prv|--play|--stop]"
+  exit 1
+  ;;
 esac
--- .config/river/scripts/waybar-toggle.sh
+++ .config/river/scripts/waybar-toggle.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
 if pgrep -x waybar >/dev/null; then
-    pkill waybar
-    notify-send -a center_notify "Waybar" "Disabled" -t 2000
+  pkill waybar
+  notify-send -a center_notify "Waybar" "Disabled" -t 2000
 else
-    waybar &
+  waybar &
 fi
