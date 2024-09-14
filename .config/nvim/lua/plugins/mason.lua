fb30988fed1989be9683825411e9a8418b431143
bitterhalt <bazz@void.com>
2024-09-14 13:32:22 +0300

    ..

--- .config/nvim/lua/plugins/mason.lua
+++ .config/nvim/lua/plugins/mason.lua
@@ -19,6 +19,16 @@ return {
           },
         },
       })
+      mason_tool_installer.setup({
+        ensure_installed = {
+          "prettier", -- prettier formatter
+          "stylua", -- lua formatter
+          "isort", -- python formatter
+          "black", -- python formatter
+          "pylint", -- python linter
+          "eslint_d", -- js linter
+        },
+      })
     end,
   },
 }
