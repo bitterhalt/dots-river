9eb7f5634ba00dd4add9fc856f5a02d42c2d76f8
bitterhalt <bazz@void.com>
2024-09-14 15:31:21 +0300

    yazi 	modified:   .config/yazi/package.toml     modified:   .config/yazi/plugins/full-border.yazi/init.lua

--- .config/yazi/plugins/full-border.yazi/init.lua
+++ .config/yazi/plugins/full-border.yazi/init.lua
@@ -9,12 +9,7 @@ local function setup(_, opts)
 			end
 
 			return ui.Bar(
-				ui.Rect({
-					x = x,
-					y = math.max(0, y),
-					w = ya.clamp(0, self._area.w - x, 1),
-					h = math.min(1, self._area.h),
-				}),
+				ui.Rect { x = x, y = math.max(0, y), w = ya.clamp(0, self._area.w - x, 1), h = math.min(1, self._area.h) },
 				ui.Bar.TOP
 			):symbol(c)
 		end
