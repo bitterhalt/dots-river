d536de0e82211aa0581888c73f378002a86c4d94
bitterhalt <bazz@void.com>
2024-09-17 23:02:39 +0300

    ..

--- .config/nvim/lua/plugins/ui.lua
+++ .config/nvim/lua/plugins/ui.lua
@@ -58,7 +58,7 @@ return {
           persist_buffer_sort = true,
           separator_style = "thin",
           enforce_regular_tabs = true,
-          always_show_bufferline = false,
+          always_show_bufferline = true,
         },
       })
     end,
