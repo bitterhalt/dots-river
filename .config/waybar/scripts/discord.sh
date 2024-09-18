04004a15a6b61dd7ed79f50856f1657c49d835fa
bitterhalt <bazz@void.com>
2024-09-18 13:49:56 +0300

    add vesktop

--- .config/waybar/scripts/discord.sh
+++ .config/waybar/scripts/discord.sh
@@ -2,7 +2,8 @@
 
 # If you use non flatpak version you most likely have to edit line 5 and scratchpads.sh in River directory
 
-if pgrep -x "com.discordapp." >/dev/null; then
+#if pgrep -x "com.discordapp." >/dev/null; then
+if pgrep -x "vesktop.bin" >/dev/null; then
   echo "{\"text\": \"\", \"tooltip\": \"<b>Discord is running in hidden workspace</b>\n left or  + F6 to open workspace\n middle to close Discord\"}"
 else
   echo ""
