c05a7199fda3694184f57a216d81c7e6be27015e
bitterhalt <bthalt@disroot.org>
2024-10-27 17:56:29 +0200

    use waybar

--- .config/river/scripts/barhider.sh
+++ .config/river/scripts/barhider.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 
-if pgrep i3bar-river >/dev/null; then
-  pkill i3bar-river
+if pgrep -x waybar >/dev/null; then
+  pkill waybar
   notify-send -a center_notify "Bar" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
 else
-  i3bar-river &
+  waybar &
 fi
