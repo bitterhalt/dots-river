8c8ba2eaeab0a9fcd0fae07be2ff38d84e98b2f1
bitterhalt <bthalt@disroot.org>
2024-11-14 20:20:17 +0200

    2nd attempt to fix screen power saver

--- .config/river/scripts/swayidle-toggle.sh
+++ .config/river/scripts/swayidle-toggle.sh
@@ -6,7 +6,7 @@ if pgrep swayidle >/dev/null; then
 else
   notify-send -a center_notify "Swayidle" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
   swayidle -w \
-    timeout 1800 "swaylock" \
-    timeout 3600 "wlopm --off '*'" resume "wlopm --on '*'" \
-    before-sleep "swaylock" &
+    timeout 1600 "swaylock -f" \
+    timeout 1800 "wlopm --off '*'" resume "wlopm --on '*'" \
+    before-sleep "swaylock -f" &
 fi
