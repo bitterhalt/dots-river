2fd3c185727f440de3a6d0029454a4736d50aa23
bitterhalt <bthalt@disroot.org>
2024-11-15 21:23:33 +0200

    add stuff to weather

--- .config/waybar/scripts/wttr.sh
+++ .config/waybar/scripts/wttr.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 for i in {1..5}; do
-  text=$(curl -s "https://wttr.in?format=+%c+%t+%m")
+  text=$(curl -s "https://wttr.in/$1?format=%c+%t+%m")
   if [[ $? == 0 ]]; then
     text=$(echo "$text" | sed -E "s/\s+/ /g")
-    tooltip=$(curl -s "https://wttr.in?format=+%l:+%C,+%t+(%f)")
+    tooltip=$(curl -s "https://wttr.in/$1?format=%l:+%C,+%t+(%f)+%w")
     if [[ $? == 0 ]]; then
       tooltip=$(echo "$tooltip" | sed -E "s/\s+/ /g")
       echo "{\"text\":\"$text\", \"tooltip\":\"$tooltip\"}"
