ed731793f08df252119df3122c7f5b0de64da190
bitterhalt <bthalt@disroot.org>
2024-11-17 18:01:27 +0200

    add sounds

--- .config/river/scripts/mic-toggle.sh
+++ .config/river/scripts/mic-toggle.sh
@@ -2,9 +2,16 @@
 
 set -euo pipefail
 
+# Toggle mute state
 wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+
+# Check the current state
 if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -qi 'MUTED'; then
+  # Mic is muted
+  pw-play "$HOME/.local/share/Sounds/device-removed.oga" &
   notify-send -a vol_notify "Mic" "off" -t 1500 -h string:x-canonical-private-synchronous:volume
 else
+  # Mic is unmuted
+  pw-play "$HOME/.local/share/Sounds/audio-volume-change.oga" &
   notify-send -a vol_notify "Mic" "on" -t 1500 -h string:x-canonical-private-synchronous:volume
 fi
