b78baf136fce41d41b9040a78059857da57cb414
bitterhalt <bthalt@disroot.org>
2024-11-29 08:48:10 +0200

    ...

--- .config/fastfetch/difference.sh
+++
@@ -1,9 +0,0 @@
-
-time2="12/12/2024 12:27AM"
-let current=$(date +%s)
-timestamp2=$(date -d "$time2" +%s)
-time_difference=$((timestamp2 - current))
-days_difference=$((time_difference / 86400))
-
-
-echo $days_difference
