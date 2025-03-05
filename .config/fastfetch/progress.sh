c56d48e6be8b1c36bee680f00408ac8cfaaae693
bitterhalt <bthalt@disroot.org>
2025-03-05 10:52:20 +0200

    ...

--- .config/fastfetch/progress.sh
+++
@@ -1,7 +0,0 @@
-
-time1="06/12/2024 12:27AM"
-let current=$(date +%s)
-timestamp1=$(date -d "$time1" +%s)
-time_progression=$((current - timestamp1))
-days_done=$((time_progression/ 86400))
-echo $days_done
