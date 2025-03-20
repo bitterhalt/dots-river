596e623438c5cbb8d24469474700640f590a4142
bitterhalt <bthalt@disroot.org>
2025-03-20 03:52:11 +0200

    Spawn river-tag-overlay when the waybar is off

--- .config/river/scripts/barhider.sh
+++ .config/river/scripts/barhider.sh
@@ -1,8 +1,14 @@
 #!/usr/bin/env bash
 
 if pgrep -x waybar >/dev/null; then
-  pkill waybar
+  killall -q waybar
   notify-send -a center_notify "Bar" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
+  # Launch river-tag-overlay when waybar is killed
+  ~/.config/river/scripts/river-tag-overlay.sh &
 else
+  if pgrep -f river-tag-overlay >/dev/null; then
+    killall -q river-tag-overlay
+  fi
+  # Launch waybar when river-tag-overlay is killed
   waybar &
 fi
