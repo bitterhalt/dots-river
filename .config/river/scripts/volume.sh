1833f3ec5e61d7f08f38d126f366549268cc1720
bitterhalt <bthalt@disroot.org>
2025-03-27 17:45:24 +0200

    nada

--- .config/river/scripts/volume.sh
+++ .config/river/scripts/volume.sh
@@ -6,10 +6,12 @@ case "${1:-}" in
 up)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
   wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%+
+  #  pw-play "$HOME/.local/share/Sounds/audio-volume-change.oga" &
   ;;
 down)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
   wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%-
+  #  pw-play "$HOME/.local/share/Sounds/audio-volume-change.oga" &
   ;;
 mute)
   wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
