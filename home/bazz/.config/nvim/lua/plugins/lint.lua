8abd74a4109f1007bc64442d79398b9f09a6ad71
bitterhalt <bthalt@disroot.org>
2025-04-17 12:17:44 +0300

    some minor changes and cleanups for: doas, lf, nvim, river

--- home/bazz/.config/nvim/lua/plugins/lint.lua
+++ home/bazz/.config/nvim/lua/plugins/lint.lua
@@ -11,7 +11,6 @@ return {
       javascriptreact = { "eslint_d" },
       typescriptreact = { "eslint_d" },
       svelte = { "eslint_d" },
-      python = { "pylint" },
     }
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
