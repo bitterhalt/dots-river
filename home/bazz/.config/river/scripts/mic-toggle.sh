a706ed0d48e13d7da0b2983d53327ca7c7dbf560
bitterhalt <bthalt@disroot.org>
2025-04-27 12:24:13 +0300

    -useless volume notification and tweak the locker. rest is just yadayada

--- home/bazz/.config/river/scripts/mic-toggle.sh
+++ home/bazz/.config/river/scripts/mic-toggle.sh
@@ -8,10 +8,10 @@ wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
 # Check the current state
 if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -qi 'MUTED'; then
   # Mic is muted
-  pw-play "$HOME/.local/share/Sounds/device-removed.oga" &
-  notify-send -a System "Microphone" "off" -t 1500 -h string:x-canonical-private-synchronous:volume
+  pw-play "${HOME}/.local/share/Sounds/device-removed.oga" &
+  notify-send -a System "Microphone muted" -t 1500 -h string:x-canonical-private-synchronous:volume
 else
   # Mic is unmuted
-  pw-play "$HOME/.local/share/Sounds/audio-volume-change.oga" &
-  notify-send -a System "Microphone" "on" -t 1500 -h string:x-canonical-private-synchronous:volume
+  pw-play "${HOME}/.local/share/Sounds/audio-volume-change.oga" &
+  notify-send -a System "Microphone active" -t 1500 -h string:x-canonical-private-synchronous:volume
 fi
