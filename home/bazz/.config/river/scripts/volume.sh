acb1159e198cd0a62588ae70c6166de309bb1654
bitterhalt <bthalt@disroot.org>
2025-04-27 19:02:21 +0300

    ...

--- home/bazz/.config/river/scripts/volume.sh
+++ home/bazz/.config/river/scripts/volume.sh
@@ -4,10 +4,12 @@ set -euo pipefail
 
 case "${1:-}" in
 up)
+  wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
   wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%+
   pw-play "${HOME}/.local/share/Sounds/audio-volume-change.oga" &
   ;;
 down)
+  wpctl set-mute @DEFAULT_AUDIO_SINK@ 0
   wpctl set-volume -l 1.1 @DEFAULT_AUDIO_SINK@ 5%-
   pw-play "${HOME}/.local/share/Sounds/audio-volume-change.oga" &
   ;;
