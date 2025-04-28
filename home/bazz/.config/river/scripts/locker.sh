3d7e688bcee841829052ceeb8e1e1dba4c0a9272
bitterhalt <bthalt@disroot.org>
2025-04-28 14:37:58 +0300

    ..

--- home/bazz/.config/river/scripts/locker.sh
+++ home/bazz/.config/river/scripts/locker.sh
@@ -4,6 +4,9 @@ swayidle \
   timeout 120 "wlopm --off '*'" \
   resume "wlopm --on '*'" &
 # Locks the screen immediately
-waylocker
+waylock -ignore-empty-password \
+  -init-color 0x000000 \
+  -input-color 0x0c0c0c \
+  -fail-color 0xAF3029
 # Kills last background task so idle timer doesn't keep running
 kill %%
