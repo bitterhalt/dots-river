a3318d245df2d1695c71814c42f859f0e11b2c12
bitterhalt <bthalt@disroot.org>
2025-04-28 22:14:03 +0300

    ..

--- home/bazz/.config/river/scripts/swayidle-toggle.sh
+++ home/bazz/.config/river/scripts/swayidle-toggle.sh
@@ -5,7 +5,5 @@ if pgrep swayidle >/dev/null; then
   notify-send -a System "Swayidle" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
 else
   notify-send -a System "Swayidle" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
-  swayidle -w \
-    timeout 1600 "swaylock -f" \
-    timeout 1800 "wlopm --off '*'" resume "wlopm --on '*'" &
+  swayidle -w &
 fi
