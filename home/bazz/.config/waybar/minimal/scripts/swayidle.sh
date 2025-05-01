6a20751f369b3d323e2b5274038e95eb1a10cd9a
bitterhalt <bthalt@disroot.org>
2025-05-01 14:09:49 +0300

    .

--- home/bazz/.config/waybar/minimal/scripts/swayidle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-if pgrep -x "swayidle" >/dev/null; then
-
-  echo "{\"text\": \"on\", \"tooltip\": \"<b>Swayidle is disabled</b>\n click left or press  + F11 to enable\"}"
-
-else
-  echo "{\"text\": \"off\", \"tooltip\": \"<b>Swayidle is disabled</b>\n click left or press  + F11 to enable\"}"
-fi
