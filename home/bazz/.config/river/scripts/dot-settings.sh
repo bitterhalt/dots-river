5411d5d8cfcec37ca39683bcd535f03e9f8e7481
bitterhalt <bthalt@disroot.org>
2025-05-04 18:23:38 +0300

    yambar

--- home/bazz/.config/river/scripts/dot-settings.sh
+++ home/bazz/.config/river/scripts/dot-settings.sh
@@ -4,7 +4,7 @@
 ARCHIVE="RIVER
 FOOT
 BEMENU
-WAYBAR
+YAMBAR
 ZSH
 NEOVIM
 MAKO"
@@ -17,7 +17,7 @@ BEMENU) $TERMCMD -e nvim $HOME/.local/bin/bemenu_runner ;;
 FOOT) $TERMCMD -e nvim $HOME/.config/foot/foot.ini ;;
 MAKO) $TERMCMD -e nvim $HOME/.config/mako/config ;;
 NEOVIM) $TERMCMD -e nvim $HOME/.config/nvim/init.lua ;;
-WAYBAR) $TERMCMD -e nvim $HOME/.config/waybar/config.jsonc ;;
+YAMBAR) $TERMCMD -e nvim $HOME/.config/yambar/config.yml ;;
 RIVER) $TERMCMD -e nvim $HOME/.config/river/init ;;
 ZSH) $TERMCMD -e nvim $HOME/.config/zsh/.zshrc ;;
 esac
