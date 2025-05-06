e55e2ac18be5f83f2201d2bcfbf5d107974b37dc
bitterhalt <bthalt@disroot.org>
2025-05-06 17:31:17 +0300

    change format

--- home/bazz/.config/waybar/scripts/mic.sh
+++ home/bazz/.config/waybar/scripts/mic.sh
@@ -4,5 +4,5 @@ set -euo pipefail
 if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -qi 'MUTED'; then
   echo ""
 else
-  echo "󰍬"
+  echo "{\"text\": \"󰍬\", \"tooltip\": \"<b>Microphone is active</b>\n click left or press  + F10 to disable\"}"
 fi
