fe2487e1b47299fa32b25824c0680cf1cfe08114
bitterhalt <bthalt@disroot.org>
2025-05-11 03:29:48 +0300

    edit vol icons

--- home/bazz/.config/river/scripts/mic-toggle.sh
+++ home/bazz/.config/river/scripts/mic-toggle.sh
@@ -9,9 +9,9 @@ wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
 if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -qi 'MUTED'; then
   # Mic is muted
   pw-play "${HOME}/.local/share/Sounds/device-removed.oga" &
-  notify-send -a System "Microphone muted" -t 1500 -h string:x-canonical-private-synchronous:volume
+  notify-send -i microphone-disabled-symbolic -a System "Microphone muted" -t 1500 -h string:x-canonical-private-synchronous:volume
 else
   # Mic is unmuted
   pw-play "${HOME}/.local/share/Sounds/audio-volume-change.oga" &
-  notify-send -a System "Microphone active" -t 1500 -h string:x-canonical-private-synchronous:volume
+  notify-send -i microphone-sensitivity-high-symbolic -a System "Microphone active" -t 1500 -h string:x-canonical-private-synchronous:volume
 fi
