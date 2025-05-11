ff6f55830aadc18d3cc1c47fcf3ff656ec906ade
bitterhalt <bthalt@disroot.org>
2025-05-11 19:44:57 +0300

    change color

--- home/bazz/.config/river/scripts/mic-toggle.sh
+++ home/bazz/.config/river/scripts/mic-toggle.sh
@@ -9,9 +9,9 @@ wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
 if wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | grep -qi 'MUTED'; then
   # Mic is muted
   pw-play "${HOME}/.local/share/Sounds/device-removed.oga" &
-  notify-send -i ~/.local/share/icons/feather/mic-off.svg "Microphone muted" -t 1500 -h string:x-canonical-private-synchronous:volume
+  notify-send -i ~/.local/share/icons/feather/mic-off.svg "Mic" "Muted" -t 1500 -h string:x-canonical-private-synchronous:volume
 else
   # Mic is unmuted
   pw-play "${HOME}/.local/share/Sounds/audio-volume-change.oga" &
-  notify-send -i ~/.local/share/icons/feather/mic.svg "Microphone active" -t 1500 -h string:x-canonical-private-synchronous:volume
+  notify-send -i ~/.local/share/icons/feather/mic.svg "Mic" "Active" -t 1500 -h string:x-canonical-private-synchronous:volume
 fi
