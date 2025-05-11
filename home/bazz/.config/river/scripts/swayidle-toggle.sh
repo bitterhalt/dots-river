6e0fedf3c9fafc5a7016409b5ff14873abf38ad4
bitterhalt <bthalt@disroot.org>
2025-05-11 03:38:34 +0300

    swayidle notification icon

--- home/bazz/.config/river/scripts/swayidle-toggle.sh
+++ home/bazz/.config/river/scripts/swayidle-toggle.sh
@@ -2,8 +2,8 @@
 
 if pgrep swayidle >/dev/null; then
   pkill swayidle
-  notify-send -a System "Swayidle" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
+  notify-send -i my-caffeine-on-symbolic -a System "Swayidle" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
 else
-  notify-send -a System "Swayidle" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
+  notify-send -i my-caffeine-off-symbolic -a System "Swayidle" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
   swayidle -w &
 fi
