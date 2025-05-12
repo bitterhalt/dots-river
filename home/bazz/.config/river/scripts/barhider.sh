294d6fad4d86b5901aeaf8d1f8147159a1697e84
bitterhalt <bthalt@disroot.org>
2025-05-12 14:10:08 +0300

    ..

--- home/bazz/.config/river/scripts/barhider.sh
+++ home/bazz/.config/river/scripts/barhider.sh
@@ -3,7 +3,7 @@
 # Launch river-tag-overlay when waybar is killed
 if pgrep -x waybar >/dev/null; then
   killall -q waybar
-  notify-send -a System "Bar disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
+  notify-send -t 2000 -a System -i ~/.local/share/icons/feather/x.svg "Bar disabled" -h string:x-canonical-private-synchronous:volume
   # Launch river-tag-overlay when waybar is killed
   ~/.config/river/scripts/river-tag-overlay.sh &
 else
