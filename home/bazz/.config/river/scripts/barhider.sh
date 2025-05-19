4abbbd31fb3d0aeb97af1ddac7f6f9076c1085ed
bitterhalt <bthalt@disroot.org>
2025-05-19 23:29:28 +0300

    rm traces of river tag overlay

--- home/bazz/.config/river/scripts/barhider.sh
+++ home/bazz/.config/river/scripts/barhider.sh
@@ -1,13 +1,8 @@
 #!/usr/bin/env bash
 
-# Launch river-tag-overlay when waybar is killed
 if pgrep -x waybar >/dev/null; then
   killall -q waybar
   notify-send -t 2000 -a System -i ~/.local/share/icons/feather/x.svg "Bar disabled" -h string:x-canonical-private-synchronous:volume
 else
-  if pgrep -f river-tag-overlay >/dev/null; then
-    killall -q river-tag-overlay
-  fi
-  # Launch waybar when river-tag-overlay is killed
   waybar &
 fi
