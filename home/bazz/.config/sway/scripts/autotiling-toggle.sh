fa8796c825c73058eb15506e4dd912cbe2b1ef11
bitterhalt <bthalt@disroot.org>
2025-05-22 19:34:12 +0300

    more dwm-like tiling

--- home/bazz/.config/sway/scripts/autotiling-toggle.sh
+++ home/bazz/.config/sway/scripts/autotiling-toggle.sh
@@ -4,6 +4,6 @@ if pgrep autotiling >/dev/null; then
   pkill autotiling
   notify-send -i ~/.local/share/icons/feather/check.svg "Autotiling" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
 else
-  autotiling &
+  autotiling -l 2 &
   notify-send -i ~/.local/share/icons/feather/x.svg "Autotiling" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
 fi
