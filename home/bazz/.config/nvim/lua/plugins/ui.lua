0b27870feaa5ddefe6eae97840833ba10d73e6fe
bitterhalt <bthalt@disroot.org>
2025-05-31 14:34:12 +0300

    use lualine

--- home/bazz/.config/nvim/lua/plugins/ui.lua
+++ home/bazz/.config/nvim/lua/plugins/ui.lua
@@ -7,34 +7,52 @@ return {
     end,
   },
 
-  -- Sttusline
+  -- Lualine
   {
-    "sontungexpt/sttusline",
-    dependencies = {
-      "nvim-tree/nvim-web-devicons",
-    },
-    event = { "BufEnter" },
-    config = function(_, opts)
-      require("sttusline").setup({
-        statusline_color = "#1C1B1A",
-        laststatus = 3,
-        disabled = {
-          filetypes = {
-            "NvimTree",
-            "lazy",
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    event = "VeryLazy",
+    config = function()
+      require("lualine").setup({
+        options = {
+          icons_enabled = true,
+          theme = "auto",
+          component_separators = { left = "", right = "" },
+          section_separators = { left = "", right = "" },
+          disabled_filetypes = {
+            statusline = {},
+            winbar = {},
           },
-          buftypes = {},
+          ignore_focus = {},
+          always_divide_middle = true,
+          always_show_tabline = true,
+          globalstatus = false,
+          refresh = {
+            statusline = 100,
+            tabline = 100,
+            winbar = 100,
+          },
         },
-        components = {
-          "mode",
-          "filename",
-          "git-branch",
-          "git-diff",
-          "%=",
-          "diagnostics",
-          "lsps-formatters",
-          "encoding",
+        sections = {
+          lualine_a = { "mode" },
+          lualine_b = { "branch", "diff", "diagnostics" },
+          lualine_c = { "filename" },
+          lualine_x = { "encoding", "fileformat", "filetype" },
+          lualine_y = { "progress" },
+          lualine_z = { "location" },
         },
+        inactive_sections = {
+          lualine_a = {},
+          lualine_b = {},
+          lualine_c = { "filename" },
+          lualine_x = { "location" },
+          lualine_y = {},
+          lualine_z = {},
+        },
+        tabline = {},
+        winbar = {},
+        inactive_winbar = {},
+        extensions = {},
       })
     end,
   },
