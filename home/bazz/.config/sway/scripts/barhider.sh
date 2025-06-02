07a028df958501fa866fe34e1adb1c02fdb65950
bitterhalt <bthalt@disroot.org>
2025-06-02 03:21:02 +0300

    tidy

--- home/bazz/.config/sway/scripts/barhider.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-if pgrep -x waybar >/dev/null; then
-  killall -q waybar
-  notify-send -t 2000 -a System -i ~/.local/share/icons/feather/x.svg "Bar disabled" -h string:x-canonical-private-synchronous:volume
-else
-  waybar &
-fi
