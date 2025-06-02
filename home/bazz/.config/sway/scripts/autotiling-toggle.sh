439cd910132b2120d9a89175b9d7d27dcbf3efad
bitterhalt <bthalt@disroot.org>
2025-06-02 03:27:28 +0300

    flip the icons

--- home/bazz/.config/sway/scripts/autotiling-toggle.sh
+++ home/bazz/.config/sway/scripts/autotiling-toggle.sh
@@ -2,8 +2,8 @@
 
 if pgrep autotiling >/dev/null; then
   pkill autotiling
-  notify-send -i ~/.local/share/icons/feather/check.svg "Autotiling" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
+  notify-send -i ~/.local/share/icons/feather/x.svg "Autotiling" "Disabled" -t 1500 -h string:x-canonical-private-synchronous:volume
 else
   autotiling -l 2 &
-  notify-send -i ~/.local/share/icons/feather/x.svg "Autotiling" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
+  notify-send -i ~/.local/share/icons/feather/check.svg "Autotiling" "Enabled" -t 1500 -h string:x-canonical-private-synchronous:volume
 fi
